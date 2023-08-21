5a233e27249cf9bb780c16af7f10f68b4b3fe943
vlbras <vlad.bras02@gmail.com>
2023-08-21 16:48:35 +0200

    feat: add best skills icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,20 @@
+import {
+  SiNodedotjs,
+  SiExpress,
+  SiNestjs,
+  SiTypescript,
+  SiDocker,
+  SiAmazonaws,
+  SiPostgresql,
+  SiMongodb,
+} from "react-icons/si";
+
 const Skills = () => {
-    return (
-        <div className="skills-container">
-            <h2>Best Skills</h2>
-            <div className="grid-skills">
-                {/* <div className="skill-card angular">
+  return (
+    <div className="skills-container">
+      <h2>Best Skills</h2>
+      <div className="grid-skills">
+        {/* <div className="skill-card angular">
                     <i className="fa-brands fa-angular"></i>
                     <p>Angular</p>
                 </div>
@@ -16,28 +27,28 @@ const Skills = () => {
                     <p>Vue</p>
                 </div> */}
 
-                { /* Back-end: */}
+        {/* Back-end: */}
 
-                <div className="skill-card node">
-                    <i className="fa-brands fa-node-js"></i>
-                    <p>Node</p>
-                </div>
-                <div className="skill-card express">
-                    <i className="fa-brands"></i>
-                    <p>Express</p>
-                </div>
-                <div className="skill-card nest">
-                    <i className="fa-brands"></i>
-                    <p>Nest</p>
-                </div>
-                <div className="skill-card ts">
-                    <i className="fa-brands"></i>
-                    <p>TypeScript</p>
-                </div>
+        <div className="skill-card node">
+          <SiNodedotjs />
+          <p>Node</p>
+        </div>
+        <div className="skill-card express">
+          <SiExpress />
+          <p>Express</p>
+        </div>
+        <div className="skill-card nest">
+          <SiNestjs />
+          <p>Nest</p>
+        </div>
+        <div className="skill-card ts">
+          <SiTypescript />
+          <p>TypeScript</p>
+        </div>
 
-                { /* Front-end: */}
+        {/* Front-end: */}
 
-                {/* <div className="skill-card html">
+        {/* <div className="skill-card html">
                     <i className="fa-brands fa-html5 html-icon"></i>
                     <p>HTML</p>
                 </div>
@@ -54,27 +65,27 @@ const Skills = () => {
                     <p>SASS</p>
                 </div> */}
 
-                { /* Tools: */}
+        {/* Tools: */}
 
-                <div className="skill-card docker">
-                    <i className="fa-brands fa-docker"></i>
-                    <p>Docker</p>
-                </div>
-                <div className="skill-card aws">
-                    <i className="fa-brands fa-aws"></i>
-                    <p>AWS</p>
-                </div>
-                <div className="skill-card postgre">
-                    <i className="fa-brands"></i>
-                    <p>PostgreSQL</p>
-                </div>
-                <div className="skill-card mongo">
-                    <i className="fa-brands"></i>
-                    <p>MongoDB</p>
-                </div>
-            </div>
+        <div className="skill-card docker">
+          <SiDocker />
+          <p>Docker</p>
         </div>
-    )
-}
+        <div className="skill-card aws">
+          <SiAmazonaws />
+          <p>AWS</p>
+        </div>
+        <div className="skill-card postgre">
+          <SiPostgresql />
+          <p>PostgreSQL</p>
+        </div>
+        <div className="skill-card mongo">
+          <SiMongodb />
+          <p>MongoDB</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
